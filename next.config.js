687b776f053e5637fb20699266fe38772c09917f
Alex Dmitrijev <aleksei.dmitrijev@heathmont.net>
2021-01-20 23:11:35 +0200

    add typescript support

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const withFonts = require('next-fonts')
 const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-svgr')
+const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withPlugins([withFonts, withSvgr], {
+module.exports = withPlugins([withFonts, withSvgr, withTypescript], {
   env: {
     API_URL: process.env.API_URL,
   },
