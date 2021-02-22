c774ca652fb2a07e4e5f01e51e973387b14014af
Alex Dmitrijev <aleksei.dmitrijev@heathmont.net>
2021-02-22 22:20:13 +0200

    add more content to vehicle detail

--- components/Vehicle/VehicleAdvDetails.tsx
+++ components/Vehicle/VehicleAdvDetails.tsx
@@ -12,8 +12,8 @@ type Props = {
 }
 
 type DetailProps = {
-  title: string | number
-  content: boolean | string | undefined
+  title?: any
+  content: boolean | string | undefined | number
   hideOnEmpty?: boolean
 }
 
@@ -28,6 +28,7 @@ const Detail: React.FunctionComponent<DetailProps> = ({
     return (
       <VehicleDetail
         title={title}
+        className={styles.detail}
         /* @ts-ignore */
         icon={<Checkbox small input={{ value: content }} />}
       >
@@ -35,10 +36,11 @@ const Detail: React.FunctionComponent<DetailProps> = ({
       </VehicleDetail>
     )
   }
-  return <VehicleDetail title={title}>{content}</VehicleDetail>
+  return <VehicleDetail title={title}>{content.toString()}</VehicleDetail>
 }
 
 const VehicleAdvDetails: React.FunctionComponent<Props> = ({ vehicle }) => {
+  const { t } = useTranslation()
   if (!vehicle) return null
   const {
     abs,
@@ -66,46 +68,134 @@ const VehicleAdvDetails: React.FunctionComponent<Props> = ({ vehicle }) => {
     selfDriving,
     startStop,
     sunRoof,
+    seatType,
+    lights,
+    towbar,
+    warrantyBook,
+    doors,
+    tireSize,
+    rightHandDrive,
   } = vehicle.vehicleExtras
   return (
     <div className={styles.detailContainer}>
-      <div className={styles.details}>
-        <h5>Safety</h5>
+      <section className={styles.details}>
+        <h5>{t('label.safety')}</h5>
         <div className={styles.list}>
-          <Detail title="Alarm" content={alarm} />
-          <Detail title="Central locking" content={centralLocking} />
-          <Detail title="Disability Equipment" content={disabilityEquipment} />
-          <Detail title="Immobilizer" content={immobilizer} />
-          <Detail title="Parking aid" content={parkingAid} />
-          <Detail title="Parking camera" content={parkingCamera} />
-          <Detail title="Abs" content={abs} />
-          <Detail title="Crashed" content={vehicle.crashed} hideOnEmpty />
+          <Detail title={t('label.alarm')} content={alarm} />
+          <Detail title={t('label.centralLocking')} content={centralLocking} />
+          <Detail
+            title={t('label.disabilityEquipment')}
+            content={disabilityEquipment}
+          />
+          <Detail title={t('label.immobilizer')} content={immobilizer} />
+          <Detail title={t('label.parkingAid')} content={parkingAid} />
+          <Detail title={t('label.parkingCamera')} content={parkingCamera} />
+          <Detail title={t('label.abs')} content={abs} />
+          <Detail
+            title={t('label.crashed')}
+            content={vehicle.crashed}
+            hideOnEmpty
+          />
         </div>
-      </div>
-      <div className={styles.details}>
-        <h5>Comfort</h5>
+      </section>
+      <section className={styles.details}>
+        <h5>{t('label.comfort')}</h5>
         <div className={styles.list}>
-          <Detail title="Interior" content={vehicle.interiorColor} />
-          <Detail title="Bluetooth" content={bluetooth} />
-          <Detail title="Cd player" content={cdPlayer} />
-          <Detail title="Climate Control" content={climateControl} />
-          <Detail title="Cruise Control" content={cruiseControl} />
-          <Detail title="Electric Adj Seats" content={electricAdjSeats} />
-          <Detail title="Electric Windows" content={electricWindows} />
-          <Detail title="Hands free" content={handsFree} />
-          <Detail title="Memory Seats" content={memorySeats} />
-          <Detail title="Memory Wheel" content={memoryWheel} />
-          <Detail title="Mirror Heating" content={mirrorHeating} />
-          <Detail title="Mirror Link" content={mirrorLink} hideOnEmpty />
-          <Detail title="Nav System" content={navSystem} />
-          <Detail title="Panorama Roof" content={panoramaRoof} />
-          <Detail title="Rain Sensor" content={rainSensor} />
-          <Detail title="Seat Heating" content={seatHeating} />
-          <Detail title="Self Driving" content={selfDriving} />
-          <Detail title="Start-stop" content={startStop} />
-          <Detail title="Sun Roof" content={sunRoof} />
+          <Detail
+            title={t('label.interiorColor')}
+            content={vehicle.interiorColor}
+          />
+          <Detail title={t('label.bluetooth')} content={bluetooth} />
+          <Detail title={t('label.cdPlayer')} content={cdPlayer} />
+          <Detail title={t('label.climateControl')} content={climateControl} />
+          <Detail title={t('label.cruiseControl')} content={cruiseControl} />
+          <Detail
+            title={t('label.ElectricAdjSeats')}
+            content={electricAdjSeats}
+          />
+          <Detail
+            title={t('label.electricWindows')}
+            content={electricWindows}
+          />
+          <Detail title={t('label.handsFree')} content={handsFree} />
+          <Detail title={t('label.memorySeats')} content={memorySeats} />
+          <Detail title={t('label.memoryWheel')} content={memoryWheel} />
+          <Detail title={t('label.mirrorHeating')} content={mirrorHeating} />
+          <Detail
+            title={t('label.mirrorLink')}
+            content={mirrorLink}
+            hideOnEmpty
+          />
+          <Detail title={t('label.navSystem')} content={navSystem} />
+          <Detail title={t('label.panoramaRoof')} content={panoramaRoof} />
+          <Detail title={t('label.rainSensor')} content={rainSensor} />
+          <Detail title={t('label.seatHeating')} content={seatHeating} />
+          <Detail title={t('label.selfDriving')} content={selfDriving} />
+          <Detail title={t('label.startStop')} content={startStop} />
+          <Detail title={t('label.sunRoof')} content={sunRoof} />
+          <Detail title={t('label.seatType')} content={seatType} />
         </div>
-      </div>
+      </section>
+      <section className={styles.details}>
+        <h5>{t('label.technical')}</h5>
+        <div className={styles.list}>
+          <Detail
+            title={t('label.emissionStandard')}
+            content={vehicle.emissionStandard}
+          />
+          <Detail
+            title={t('label.fuelTankCapacity')}
+            content={vehicle.fuelTankCapacity}
+          />
+          <Detail title={t('label.height')} content={vehicle.height} />
+          <Detail title={t('label.weight')} content={vehicle.weight} />
+          <Detail title={t('label.tireSize')} content={tireSize} />
+          <Detail
+            title={t('label.rightHandDrive')}
+            content={rightHandDrive}
+            hideOnEmpty
+          />
+          <Detail title={t('label.capacity')} content={vehicle.capacity} />
+        </div>
+      </section>
+      <section className={styles.details}>
+        <h5>{t('label.other')}</h5>
+        <div className={styles.list}>
+          <Detail title={t('label.vin')} content={vehicle.VIN} />
+          <Detail title={t('label.regNumber')} content={vehicle.regNumber} />
+          <Detail title={t('label.warrantyBook')} content={warrantyBook} />
+          <Detail
+            title={t('label.consumptionCombined')}
+            content={vehicle.consumptionCombined}
+          />
+          <Detail
+            title={t('label.consumptionHighway')}
+            content={vehicle.consumptionHighway}
+          />
+          <Detail
+            title={t('label.consumptionUrban')}
+            content={vehicle.consumptionUrban}
+          />
+        </div>
+      </section>
+      <section className={styles.details}>
+        <h5>{t('label.lights')}</h5>
+        <div className={styles.list}>
+          <Detail title={t('label.lights')} content={lights} />
+        </div>
+      </section>
+      <section className={styles.details}>
+        <h5>{t('label.exterior')}</h5>
+        <div className={styles.list}>
+          <Detail title={t('label.towbar')} content={towbar} />
+          <Detail title={t('label.doors')} content={doors} hideOnEmpty />
+          <Detail title={t('label.color')} content={vehicle.color} />
+          <Detail
+            title={t('label.metallicColor')}
+            content={vehicle.metallicColor}
+          />
+        </div>
+      </section>
     </div>
   )
 }
--- components/Vehicle/VehicleDetail.tsx
+++ components/Vehicle/VehicleDetail.tsx
@@ -1,21 +1,24 @@
 import React, { ReactChildren, ReactChild } from 'react'
 import { FiTag } from 'react-icons/fi'
 import { IconType } from 'react-icons/lib'
+import classNames from 'classnames'
 import styles from './VehicleDetail.module.scss'
 
 type Props = {
   icon?: IconType | ReactChild | ReactChildren
   title?: any
   children?: any
+  className?: string
 }
 
 const VehicleDetail: React.FunctionComponent<Props> = ({
   icon,
   title,
   children,
+  className
 }) => {
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.container, className)}>
       {title && <h6>{title}</h6>}
       <div className={styles.detail}>
         {icon ? icon : <FiTag />}
