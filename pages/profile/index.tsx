175314607d4382fe1c4ad5d4b79cd238f937f603
Alex Dmitrijev <aleksei.dmitrijev@heathmont.net>
2021-03-01 23:15:58 +0200

    add self profile id

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import get from 'lodash/get'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+import { currentUserSelector } from '../../store/auth/selectors'
 
-const ProfilePage = () => {
-  return <div>Profile page</div>
+const ProfilePage: React.FunctionComponent = () => {
+  const currentUser = useSelector(currentUserSelector)
+  const { push } = useRouter()
+  const userId = get(currentUser, '_id')
+
+  useEffect(() => {
+    push(userId ? `/profile/${userId}` : '/')
+  }, [userId])
+
+  return null
 }
 
 export default ProfilePage
